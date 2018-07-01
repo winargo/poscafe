4d388a08df4ae58d8ff14b9b4668beb2ee0fdab0
DannyTandean <alecia_clair00@yahoo.com>
2018-07-01 11:50:52 +0700

    orders

--- orders.php
+++ orders.php
@@ -9,17 +9,17 @@
     <title>User Management</title>
     <!--    boostrap css-->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    
+
     <!--    custom-->
     <link rel="stylesheet" href="./css/order.css">
-    
+
     <!-- Favicon-->
     <link rel="icon" href="./favicon.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
-    
+
     <script type="text/javascript" src="./bootstrap/js/jquery.js"></script>
     <script type="text/javascript" src="./bootstrap/js/bootstrap.js"></script>
     <!--    bootstrap-->
@@ -28,7 +28,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 -->
-    
+
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color:orange">
@@ -39,7 +39,7 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            &nbsp; 
+            &nbsp;
         </ul>
         <div class="btn-group float-right"  style="margin-right : 5%;">
              <button class="btn btn-success" data-toggle="modal" data-target="#exampleModal">Add Menu</button>
@@ -57,7 +57,7 @@
                   ?>
                 <a class="dropdown-item" href="./action/logout.php">Logout</a>
               </div>
-              
+
               <!-- Modal -->
                <script>
                        function readURL(input) {
@@ -85,7 +85,7 @@
                       </div>
 <!--                      form-->
                       <form action="cekmenu.php" method="post" id="uploadimage" enctype="multipart/form-data">
-                          
+
                       <div class="modal-body">
                         <div class="form-group">
                            <label for="1">Product Code</label>
@@ -98,20 +98,20 @@
                         <div class="form-group">
                             <label for="4">Select Category</label>
                             <select name="selcate" id="4" class="form-control" >
-                               <?php 
+                               <?php
                                     include "./config/connection.php";
-                                
-                                    $sql = "select * from category";
+
+                                    $sql = "select * from iamproduk";
                                     $query = mysqli_query($conn,$sql);
-                                    
+
                                 while($row = mysqli_fetch_array($query))
                                 {
                                     ?>
-                                    <option value="<?php echo $row["category_code"]; ?>"><?php echo $row["category_name"];?></option>
-                                    
+                                    <option value="<?php echo $row["KODE_PRODUK"]; ?>"><?php echo $row["NAMA_PRODUK"];?></option>
+
                                     <?php
                                 }
-                                    
+
                                 ?>
                             </select>
                         </div>
@@ -124,7 +124,7 @@
                           </div>
                         <div class="form-group">
                                <img src="./images/images.jpg" id="imageemergency" width="300px" height="200px">
-                               
+
                             </div>
                             <div class="form-group">
                                 <input type="file" name="fileToUpload" id="fileToUpload"  onchange="readURL(this);" required>
@@ -146,7 +146,7 @@
     <!-- #Top Bar -->
 <!--
     <div class="wrapper item">
-       <div class="menureceipt"> 
+       <div class="menureceipt">
            <button type="button" class="btn btn-default waves-effect m-r-20" data-toggle="modal" data-target="#defaultModal" style="float:right;margin-top:-30px;">+</button>
             <div class="catalog" style="margin-top:10px;">
                 <div class="boxitem">
@@ -161,7 +161,7 @@
                 <div class="boxadd">
                     <img src="./images/add.png" class="additemimage" >
                  </div>
-            </div> 
+            </div>
         </div>
         <div class="receipt">
             <div class="receipt-data">
@@ -184,7 +184,7 @@
                 <div class="checkout">
                     <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-print"> Check Out</button>
                 </div>
-            </div> 
+            </div>
         </div>
 
     </div>
@@ -192,44 +192,44 @@
     <div class="row">
         <div class="col-md-7">
             <div class="row" style="text-align:center;">
-                <?php 
+                <?php
                     include "./config/connection.php";
-                    
-                    $sql = "select * from iamstock where tdk_aktif='0'";
+
+                    $sql = "select * from iamstock where tdk_aktif=0";
                     $query = mysqli_query($conn,$sql);
                     while($row = mysqli_fetch_array($query)){
                         ?>
                     <div class="col-md-2" style="background-color:orange;border-radius:5px;">
                         <img src="./images/logo.PNG" alt="logo" style="width:100%;">
                         <h6><?php echo $row["NAMA_STOCK"] ?></h6>
-                    </div>        
+                    </div>
                 <?php
                     }
-                ?> 
+                ?>
             </div>
         </div>
-        
+
         <div class="col-md-4" style="background-color:orange;">
              <div class="checkoutdata">
-                 
+
                   <div class="menu-head">
-                     <img src="./images/logo.PNG" width="150" height="150">     
+                     <img src="./images/logo.PNG" width="150" height="150">
                      <p>Address<br>Phone Number Data<br>Additional Data</p>
                      <p style="text-align:left;">
                         No : nomor
                         <span style="float:right;"><?php
                             echo date("d/m/Y");
-                        
+
                             ?></span>
                      </p>
                      <p style="text-align:left;">
                         Cashier : <?php echo $_SESSION['username'] ; ?>
                         <span style="float:right;"><?php
                             echo date("h:i:s");
-                        
+
                             ?></span>
                      </p>
-                     
+
                      <hr style="height: 1px;background-color:black;">
                      <div class="menu-item">
                          <p style="text-align:left;">data menu</p>
@@ -247,28 +247,33 @@
                          <p style="text-align:left;">data menu</p>
                          <div style="text-align:left;"> <div style="display: inline-block;min-width:40px;">100</div> <div style="display: inline-block;min-width:80px;">bowl&nbsp;&nbsp;&nbsp;X</div><div style="display: inline-block;min-width:100px;">45,000</div><div style="display: inline-block;min-width:50px;">5%</div><div style="text-align:right;float:right;min-width:120px;">6,000,000</div>  </div>
                      </div>
-                     
+
                      <hr style="height: 1px;background-color:black;">
-                     
+
                      <p style="text-align:left;">
-                        Disc    
+                        Disc
                         <span style="float:right;min-width:200px;text-align:right;">Value</span>
                         <span style="float:right;">=</span>
                      </p>
                      <p style="text-align:left;">
-                        Total     
+                        Total
                         <span style="float:right;min-width:200px;text-align:right;">Value</span>
                         <span style="float:right;">=</span>
                      </p>
                   </div>
-                   
+
                </div>
                 <div class="checkout">
                     <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-print"></span> Check Out</button>
                 </div>
-            </div> 
+            </div>
         </div>
 </body>
 
-    
+<script type="text/javascript">
+$('#exampleModal').on('shown.bs.modal', function () {
+$('.modal fade').removeClass();
+})
+
+</script>
 </html>
