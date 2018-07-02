49c6de5f3c6673764bb91d9afd4c7b3cbacfdc70
DannyTandean <alecia_clair00@yahoo.com>
2018-07-02 22:10:37 +0700

    orderqtycheckout

--- action/cekcheckout.php
+++ action/cekcheckout.php
@@ -7,13 +7,34 @@ $qty = $_POST['qty'];
 $userid = $_POST['userid'];
 $harga = $_POST['harga'];
 
-$query = "insert into cart (KODE_STOCK,QTY,UNIT,HARGA,user_id) value ('$namastock','$qty','pcs','$harga','$userid')";
-$sql  = mysqli_query($conn,$query);
+$q1 = "select * from cart where checkout_status = 0";
+$s1 = mysqli_query($conn,$q1);
 
-if($sql)
-{
-  echo "TRUE";
-  header("Location: ../orders.php");
-  exit();
+while ($row = mysqli_fetch_array($s1)) {
+  if ($row["KODE_STOCK"] == $namastock) {
+      $s2 = "update cart set QTY = QTY + 1 where KODE_STOCK = '$namastock'";
+      $q2 = mysqli_query($conn,$s2);
+      if($q2)
+      {
+        header("Location: ../orders.php");
+        exit();
+      }
+  }
+  else{
+    echo "Error";
+  }
 }
+
+  $query = "insert into cart (KODE_STOCK,QTY,UNIT,HARGA,user_id) value ('$namastock','$qty','pcs','$harga','$userid')";
+  $sql  = mysqli_query($conn,$query);
+
+  if($sql)
+  {
+    echo "TRUE";
+    header("Location: ../orders.php");
+    exit();
+  }
+  else {
+    echo "error 2";
+  }
  ?>
