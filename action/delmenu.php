ffed3dc1c298cf6f943aacec0f91fe7ff2855f00
DannyTan <alecia_clair00@yahoo.com>
2018-07-02 18:15:06 +0800

    editmenuv2

--- action/delmenu.php
+++ action/delmenu.php
@@ -12,20 +12,20 @@ $ketemu=mysqli_num_rows($cekcategory);
 
 
 if ($ketemu == 1){
-  
+
     $sql = "delete from iamstock where KODE_STOCK='$kodestock'";
     $query = mysqli_query($conn,$sql);
-    
+
     if($query)
-    {        
-        header ("Location: ..\viewmenu.php");
+    {
+        header ("Location: ../viewmenu.php");
         exit();
     }
     else{
         echo "gagal ";
     }
 }else{
-    
+
 }
 
 
